37c5f66ff148004058700e56588734793257d655
DavidVale0526 <michaell.mercado21518408@estu.unan.edu.ni>
2024-10-31 19:34:10 -0600

    Se agrego la funcion de buscar en la pagina Venta.xaml para buscar producgtos

--- App4/views/Venta.xaml.cs
+++ App4/views/Venta.xaml.cs
@@ -9,14 +9,17 @@ namespace App4.views;
 public partial class Venta : ContentPage
 {
     private string firebaseURL = "https://crud-bootstrap-ab39a-default-rtdb.firebaseio.com/";
-    private ObservableCollection<Sales> salesList;
-    private FirebaseClient firebaseClient;
+    private ObservableCollection<Sales> salesList; // Lista para almacenar las ventas
+    private ObservableCollection<Sales> allSalesList; // Nueva lista para almacenar todas las ventas
+
+    private FirebaseClient firebaseClient;// Cliente de Firebase
 
     public Venta()
     {
         InitializeComponent();
         firebaseClient = new FirebaseClient(firebaseURL);
         salesList = new ObservableCollection<Sales>();
+        allSalesList = new ObservableCollection<Sales>();
         salesCollectionView.ItemsSource = salesList; // Asegúrate de que esto esté configurado
     }
 
@@ -28,7 +31,9 @@ public partial class Venta : ContentPage
 
     private async Task LoadSales()
     {
+        allSalesList.Clear();
         salesList.Clear();
+
         var sales = await firebaseClient
             .Child("sales")
             .OnceAsync<object>();
@@ -37,6 +42,7 @@ public partial class Venta : ContentPage
         {
             var saleData = JsonConvert.DeserializeObject<Sales>(sale.Object.ToString());
             saleData.Id = sale.Key;
+            allSalesList.Add(saleData);
             salesList.Add(saleData);
         }
     }
@@ -74,5 +80,32 @@ public partial class Venta : ContentPage
     {
         var sale = (Sales)((TappedEventArgs)e).Parameter; // Obtener la venta seleccionada
         await Navigation.PushAsync(new EditarVenta(sale)); // Navegar a la página de edición
+    }
+
+    //metodos para buscar ventas
+    private void OnSearchTextChanged(object sender, TextChangedEventArgs e)
+    {
+        string searchText = e.NewTextValue?.ToLower() ?? string.Empty;
+        FilterSales(searchText);
+    }
+
+    private void FilterSales(string searchText)
+    {
+        // Limpiar la lista actual
+        salesList.Clear();
+
+        // Cargar todas las ventas desde Firebase
+        LoadSales().ContinueWith(task =>
+        {
+            // Filtrar las ventas según el texto de búsqueda
+            var filteredSales = salesList.Where(sale => sale.NombreProducto.ToLower().Contains(searchText)).ToList();
+
+            // Limpiar la lista y agregar las ventas filtradas
+            salesList.Clear();
+            foreach (var sale in filteredSales)
+            {
+                salesList.Add(sale);
+            }
+        }, TaskScheduler.FromCurrentSynchronizationContext());
     }
 }
