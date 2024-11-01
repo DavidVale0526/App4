887b0850f2293b9c61c017067709fb6dd89e38e3
DavidVale0526 <michaell.mercado21518408@estu.unan.edu.ni>
2024-11-01 03:12:54 -0600

    Se muestra la carga en el inicio de sesion

--- App4/MainPage.xaml.cs
+++ App4/MainPage.xaml.cs
@@ -15,35 +15,43 @@ namespace App4
             string email = emailEntry.Text;
             string password = passwordEntry.Text;
 
-            // Validación: Verificar si el campo de correo electrónico está vacío
+            // Validaciones
             if (string.IsNullOrWhiteSpace(email))
             {
                 await DisplayAlert("Error", "Por favor, ingrese un correo electrónico.", "Ok");
                 return;
             }
-
-            // Validación: Verificar si el campo de contraseña está vacío
             if (string.IsNullOrWhiteSpace(password))
             {
                 await DisplayAlert("Error", "Por favor, ingrese una contraseña.", "Ok");
                 return;
             }
 
+            // Mostrar el indicador de carga y deshabilitar el botón
+            loadingIndicator.IsVisible = true;
+            loadingIndicator.IsRunning = true;
+            loginButton.IsEnabled = false;
+
             try
             {
                 ConexionFirebase conexionFirebase = new ConexionFirebase();
                 var Credenciales = await conexionFirebase.CargarUsuario(email, password);
                 var Uid = Credenciales.User.Uid;
-
                 await DisplayAlert("Inicio de Sesión Exitoso", $"Usuario con UID: {Uid}", "Ok");
 
-                //navegar a la página de home
+                // Navegar a la página de home
                 await Navigation.PushAsync(new Venta());
             }
             catch (Exception ex)
             {
-                // Manejo de excepciones: Mostrar mensaje de error si ocurre una excepción
                 await DisplayAlert("Error", $"Ocurrió un error: {ex.Message}", "Ok");
+            }
+            finally
+            {
+                // Ocultar el indicador de carga y habilitar el botón
+                loadingIndicator.IsVisible = false;
+                loadingIndicator.IsRunning = false;
+                loginButton.IsEnabled = true;
             }
         }
 
