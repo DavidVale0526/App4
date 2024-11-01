58952ecddb892ab0d2a5566a74fc8a2177ea925e
DavidVale0526 <michaell.mercado21518408@estu.unan.edu.ni>
2024-11-01 02:45:01 -0600

    Se agregaron las funcionalidades de cliente y proveedores

--- App4/views/Cliente.xaml.cs
+++ App4/views/Cliente.xaml.cs
@@ -1,9 +1,58 @@
-namespace App4.views;
+using Firebase.Database;
+using Firebase.Database.Query;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
 
-public partial class Cliente : ContentPage
+namespace App4.views
 {
-	public Cliente()
-	{
-		InitializeComponent();
-	}
+    public partial class Cliente : ContentPage
+    {
+        private string firebaseURL = "https://crud-bootstrap-ab39a-default-rtdb.firebaseio.com/";
+        private FirebaseClient firebaseClient;
+
+        public Cliente()
+        {
+            InitializeComponent();
+            firebaseClient = new FirebaseClient(firebaseURL);
+        }
+
+        private async void OnAgregarClienteClicked(object sender, EventArgs e)
+        {
+            string nombre = nombreEntry.Text;
+            string email = emailEntry.Text;
+            string telefono = telefonoEntry.Text;
+
+            if (string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(telefono))
+            {
+                await DisplayAlert("Error", "Todos los campos son obligatorios", "OK");
+                return;
+            }
+
+            var cliente = new ClienteModel
+            {
+                Id = Guid.NewGuid().ToString(),
+                Nombre = nombre,
+                Email = email,
+                Telefono = telefono,
+                FechaRegistro = DateTime.Now.ToString("dd/MM/yyyy HH:mm")
+            };
+
+            await firebaseClient
+                .Child("clientes")
+                .PostAsync(JsonConvert.SerializeObject(cliente));
+
+            await DisplayAlert("Mensaje", "Cliente agregado correctamente", "OK");
+            await Navigation.PopAsync();
+        }
+    }
+
+    public class ClienteModel
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public string FechaRegistro { get; set; }
+    }
 }
--- App4/views/Proveedor.xaml.cs
+++ App4/views/Proveedor.xaml.cs
@@ -1,9 +1,59 @@
-namespace App4.views;
+// Proveedor.xaml.cs
+using Firebase.Database;
+using Firebase.Database.Query;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
 
-public partial class Proveedor : ContentPage
+namespace App4.views
 {
-	public Proveedor()
-	{
-		InitializeComponent();
-	}
+    public partial class Proveedor : ContentPage
+    {
+        private string firebaseURL = "https://crud-bootstrap-ab39a-default-rtdb.firebaseio.com/";
+        private FirebaseClient firebaseClient;
+
+        public Proveedor()
+        {
+            InitializeComponent();
+            firebaseClient = new FirebaseClient(firebaseURL);
+        }
+
+        private async void OnAgregarProveedorClicked(object sender, EventArgs e)
+        {
+            string nombre = nombreEntry.Text;
+            string empresa = empresaEntry.Text;
+            string contacto = contactoEntry.Text;
+
+            if (string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(empresa) || string.IsNullOrWhiteSpace(contacto))
+            {
+                await DisplayAlert("Error", "Todos los campos son obligatorios", "OK");
+                return;
+            }
+
+            var proveedor = new ProveedorModel
+            {
+                Id = Guid.NewGuid().ToString(),
+                Nombre = nombre,
+                Empresa = empresa,
+                Contacto = contacto,
+                FechaRegistro = DateTime.Now.ToString("dd/MM/yyyy HH:mm")
+            };
+
+            await firebaseClient
+                .Child("proveedores")
+                .PostAsync(JsonConvert.SerializeObject(proveedor));
+
+            await DisplayAlert("Mensaje", "Proveedor agregado correctamente", "OK");
+            await Navigation.PopAsync();
+        }
+    }
+
+    public class ProveedorModel
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Empresa { get; set; }
+        public string Contacto { get; set; }
+        public string FechaRegistro { get; set; }
+    }
 }
